1be69006e56ef853f00de50589d0a3f999b55256
Alexey Poryadin <alexey.poryadin@gmail.com>
2013-07-04 22:26:29 +0400

    Initialize the repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    version = "0.1",
+    name = "pyGibson",
+    description = "The client for Gibson key-value storage.",
+    packages = find_packages(),
+    install_requires = ['tornado>=3.0'],
+)
+
